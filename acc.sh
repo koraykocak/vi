7f49b44068779806534332ecd4036d2f7bb85f4b
Koray Kocak <koraykocak@gmail.com>
2015-12-22 10:22:41 +0200

    disabled acceleration also for touchpad. increased touchpad speed

--- acc.sh
+++ acc.sh
@@ -3,14 +3,21 @@
 #wait for the desktop to settle
 # sleep 5
 
-#gets the hardware id's of all mice plugged into the system
-hardwareIds=$(xinput | grep -i 'Logitech\|mouse' | awk '{print substr($(NF-3),4)}')
+#gets the hardware id's of all mice and touchpads plugged into the system
+hardwareIds=$(xinput | grep -i 'Logitech\|mouse\|touchpad' | awk '{print substr($(NF-3),4)}')
 
-#turn off mouse acceleration
+#turn off mouse and touchpad acceleration
 for i in $hardwareIds
 do
 xinput set-prop ${i} 'Device Accel Profile' -1
 xinput set-prop ${i} 'Device Accel Velocity Scaling' 1
+done
+
+# increase touchpad speed
+hardwareIds_touchpad=$(xinput | grep -i 'touchpad' | awk '{print substr($(NF-3),4)}')
+for i in $hardwareIds_touchpad
+do
+xinput set-prop ${i} 'Device Accel Constant Deceleration' 1
 done
 
 
